ec383ad030f2ffcb85601463c82c79be2d24a138
gauravsharma <21275839+gaurravsh@users.noreply.github.com>
2023-02-13 09:08:44 +0530

    first commit

---
+++ src/main/java/com/shaga/springcloudconfigsever/SpringCloudConfigSeverApplication.java
@@ -0,0 +1,15 @@
+package com.shaga.springcloudconfigsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigSeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaga/springcloudconfigsever/SpringCloudConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaga.springcloudconfigsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
